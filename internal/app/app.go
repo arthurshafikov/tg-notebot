4f4ad3c704eadb18486091d031284da5107c80ca
thewolf27 <wolf-front@yandex.ru>
2022-04-04 07:24:51 +0400

    add basic initialization

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,8 +2,16 @@ package app
 
 import (
 	"context"
+
+	"github.com/thewolf27/wolf-notebot/internal/services"
+	server "github.com/thewolf27/wolf-notebot/internal/transport/http"
+	handler "github.com/thewolf27/wolf-notebot/internal/transport/http/v1"
 )
 
 func Run() {
-	_ = context.Background()
+	ctx := context.Background()
+
+	handler := handler.NewHandler(ctx, services.NewServices())
+	s := server.NewServer(handler)
+	s.Serve(ctx, "8123")
 }
