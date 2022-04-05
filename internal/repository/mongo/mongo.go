952c6fa5f81e096f12ab0ec7db1c4dc6000d0cf7
thewolf27 <wolf-front@yandex.ru>
2022-04-05 07:55:27 +0400

    fix mongo connection errors

--- internal/repository/mongo/mongo.go
+++ internal/repository/mongo/mongo.go
@@ -9,14 +9,25 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func NewMongoDB(ctx context.Context, host, port string) (*mongo.Client, error) {
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		fmt.Sprintf("mongodb://%s:%s", host, port),
-	))
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+}
+
+func NewMongoDB(ctx context.Context, config Config) (*mongo.Client, error) {
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI(
+			fmt.Sprintf("mongodb://%s:%s", config.Host, config.Port),
+		).SetAuth(options.Credential{Username: config.Username, Password: config.Password}),
+	)
 	if err != nil {
 		return nil, err
 	}
-	defer func() {
+	go func() {
+		<-ctx.Done()
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
 		}
