bf1a2b22edcce9f729178424e3d8f842b07c3405
thewolf27 <wolf-front@yandex.ru>
2022-04-08 07:19:50 +0400

    embed categories in user model

--- internal/core/category.go
+++ internal/core/category.go
@@ -3,8 +3,7 @@ package core
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Category struct {
-	ID     primitive.ObjectID `bson:"_id"`
-	UserId int                `bson:"user_id"`
-	Name   string             `bson:"name"`
-	Notes  []Note             `bson:"notes"`
+	ID    primitive.ObjectID `bson:"_id"`
+	Name  string             `bson:"name"`
+	Notes []Note             `bson:"notes"`
 }
--- internal/core/user.go
+++ internal/core/user.go
@@ -3,7 +3,8 @@ package core
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID        primitive.ObjectID `bson:"_id"`
-	Name      string             `bson:"name"`
-	SaltToken string             `bson:"salt_token"`
+	ID         primitive.ObjectID `bson:"_id"`
+	Name       string             `bson:"name"`
+	SaltToken  string             `bson:"salt_token"`
+	Categories []Category         `bson:"categories"`
 }
