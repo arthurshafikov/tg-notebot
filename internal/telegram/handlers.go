68b28df047cb98d3695167a8623e22517c316095
arthurshafikov <arthur.shafikov27@gmail.com>
2022-04-16 08:15:50 +0400

    add addcategory handler

--- internal/telegram/handlers.go
+++ internal/telegram/handlers.go
@@ -1,11 +1,14 @@
 package telegram
 
 import (
+	"fmt"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 var (
-	startCommand = "/start"
+	startCommand       = "start"
+	addCategoryCommand = "addcategory"
 )
 
 func (b *TelegramBot) handleMessage(message *tgbotapi.Message) error {
@@ -16,9 +19,11 @@ func (b *TelegramBot) handleMessage(message *tgbotapi.Message) error {
 }
 
 func (b *TelegramBot) handleCommand(message *tgbotapi.Message) error {
-	switch message.Text {
+	switch message.Command() {
 	case startCommand:
 		return b.handleStart(message)
+	case addCategoryCommand:
+		return b.handleAddCategory(message)
 	}
 
 	return nil
@@ -30,6 +35,19 @@ func (b *TelegramBot) handleStart(message *tgbotapi.Message) error {
 	}
 
 	msg := tgbotapi.NewMessage(message.Chat.ID, `Successfully authorized!`)
+	_, err := b.bot.Send(msg)
+
+	return err
+}
+
+func (b *TelegramBot) handleAddCategory(message *tgbotapi.Message) error {
+	categoryName := message.CommandArguments()
+
+	if err := b.services.Categories.AddCategory(b.ctx, message.Chat.ID, categoryName); err != nil {
+		return err
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, fmt.Sprintf("Category %s was created successfully!", categoryName))
 	_, err := b.bot.Send(msg)
 
 	return err
